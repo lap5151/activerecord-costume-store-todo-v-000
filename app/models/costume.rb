bcb42aac3d14de105b9f8653ae2866c420f6fc50
Lauren Pardee <lap5151@gmail.com>
2019-02-12 23:55:37 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,13 +3,4 @@
 
 class Costume < ActiveRecord::Base
 
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.integer :size
-      t.string  :image_url
-    end
-  end
-
 end
