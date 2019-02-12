671292ecc73ad89b3fe1bc11f32a23b672bc53e0
Lauren Pardee <lap5151@gmail.com>
2019-02-12 23:01:44 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,14 @@
-# Create your CostumeStore class here
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string   :image_url
+      t.datetime  :created_at
+      t.datetime  :updated_at
+    end
+  end
+
+end
