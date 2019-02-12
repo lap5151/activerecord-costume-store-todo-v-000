1c86fb999574dd7d4f788468e1b82fbcd7dc88cf
Lauren Pardee <lap5151@gmail.com>
2019-02-12 23:52:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,8 +6,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.integer :size
       t.string   :image_url
-      t.datetime  :created_at
-      t.datetime  :updated_at
+      t.timestamps
     end
   end
 
