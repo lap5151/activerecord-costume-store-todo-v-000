50b348d38520c7b2ae54860b6d8bc524d23b1eab
Lauren Pardee <lap5151@gmail.com>
2019-02-13 00:20:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.count :number_of_employees
+      t.integer :number_of_employees
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
