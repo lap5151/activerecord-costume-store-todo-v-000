78d15d28f5592ad8ffb901ac4b2de8ad63235df4
Lauren Pardee <lap5151@gmail.com>
2019-02-13 00:19:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,7 +11,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_freindly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :long_description
+      t.comment :long_description
     end
   end
 
